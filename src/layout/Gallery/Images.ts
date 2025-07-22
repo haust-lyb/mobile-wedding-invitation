56b7f352f9c9436346b46a2a0a9e4ac637d0da3d
yibo <1570194845@qq.com>
2025-07-22 23:36:01 +0800

    照片资源更新

--- src/layout/Gallery/Images.ts
+++ src/layout/Gallery/Images.ts
@@ -1,80 +1,155 @@
-import image01 from '@/assets/images/01.jpg'
-import image02 from '@/assets/images/02.jpg'
-import image03 from '@/assets/images/03.jpg'
-import image04 from '@/assets/images/04.jpg'
-import image05 from '@/assets/images/05.jpg'
-import image06 from '@/assets/images/06.jpg'
-import image07 from '@/assets/images/07.jpg'
-import image08 from '@/assets/images/08.jpg'
-import image09 from '@/assets/images/09.jpg'
-import image10 from '@/assets/images/10.jpg'
-import image11 from '@/assets/images/11.jpg'
-import image12 from '@/assets/images/12.jpg'
+import image01 from '@/assets/images/0L2A0232.jpg'
+import image02 from '@/assets/images/0L2A0242.jpg'
+import image03 from '@/assets/images/0L2A0246.jpg'
+import image04 from '@/assets/images/0L2A0260.jpg'
+import image05 from '@/assets/images/0L2A0265.jpg'
+import image06 from '@/assets/images/0L2A0287.jpg'
+import image07 from '@/assets/images/0L2A0289.jpg'
+import image08 from '@/assets/images/0L2A0291.jpg'
+import image09 from '@/assets/images/0L2A0295.jpg'
+import image10 from '@/assets/images/0L2A0312.jpg'
+import image11 from '@/assets/images/0L2A0315.jpg'
+import image12 from '@/assets/images/0L2A0333.jpg'
+import image13 from '@/assets/images/0L2A0334.jpg'
+import image14 from '@/assets/images/0L2A0350.jpg'
+import image15 from '@/assets/images/0L2A0352.jpg'
+import image16 from '@/assets/images/0L2A0365.jpg'
+import image17 from '@/assets/images/0L2A0388.jpg'
+import image18 from '@/assets/images/0L2A0392.jpg'
+import image19 from '@/assets/images/0L2A0394.jpg'
+import image20 from '@/assets/images/0L2A0422.jpg'
+import image21 from '@/assets/images/0L2A0497.jpg'
+import image22 from '@/assets/images/0L2A0508.jpg'
+import image23 from '@/assets/images/0L2A0515.jpg'
+import image24 from '@/assets/images/0L2A0539女生专属页.jpg'
+import image25 from '@/assets/images/0L2A0540女生专属页.jpg'
+import image26 from '@/assets/images/0L2A0548.jpg'
+import image27 from '@/assets/images/0L2A0552--36寸珍影艺术画框-纯白.jpg'
+import image28 from '@/assets/images/0L2A0557.jpg'
+import image29 from '@/assets/images/0L2A0596.jpg'
+import image30 from '@/assets/images/0L2A0604.jpg'
+import image31 from '@/assets/images/0L2A0613.jpg'
+import image32 from '@/assets/images/0L2A0616男生专属页.jpg'
+import image33 from '@/assets/images/0L2A0622.jpg'
+import image34 from '@/assets/images/0L2A0630.jpg'
+import image35 from '@/assets/images/0L2A0631男生专属页.jpg'
+import image36 from '@/assets/images/0L2A0641--20寸四宫格画框.jpg'
+import image37 from '@/assets/images/0L2A0644.jpg'
+import image38 from '@/assets/images/0L2A0650--20寸四宫格画框.jpg'
+import image39 from '@/assets/images/0L2A0655.jpg'
+import image40 from '@/assets/images/0L2A0663特写版面.jpg'
+import image41 from '@/assets/images/0L2A0667--20寸四宫格画框.jpg'
+import image42 from '@/assets/images/0L2A0669特写版面.jpg'
+import image43 from '@/assets/images/0L2A0672--20寸四宫格画框.jpg'
+import image44 from '@/assets/images/0L2A0681精品海报.jpg'
+import image45 from '@/assets/images/0L2A0687.jpg'
+import image46 from '@/assets/images/0L2A0690.jpg'
+import image47 from '@/assets/images/0L2A0705--10寸油画艺术台框-纯白.jpg'
+import image48 from '@/assets/images/0L2A0716.jpg'
+import image49 from '@/assets/images/0L2A0740.jpg'
+import image50 from '@/assets/images/0L2A0786.jpg'
+import image51 from '@/assets/images/0L2A0808.jpg'
+import image52 from '@/assets/images/0L2A0814--10寸油画艺术台框-雅黑.jpg'
+import image53 from '@/assets/images/0L2A0845.jpg'
+import image54 from '@/assets/images/0L2A0847.jpg'
+import image55 from '@/assets/images/0L2A0871.jpg'
+import image56 from '@/assets/images/0L2A0876.jpg'
+import image57 from '@/assets/images/0L2A0878.jpg'
+import image58 from '@/assets/images/0L2A0902.jpg'
+import image59 from '@/assets/images/0L2A0909.jpg'
+import image60 from '@/assets/images/0L2A0913.jpg'
+import image61 from '@/assets/images/0L2A0939--28寸珍影艺术画框-纯白.jpg'
+import image62 from '@/assets/images/0L2A0945.jpg'
+import image63 from '@/assets/images/0L2A0947.jpg'
+import image64 from '@/assets/images/0L2A0957.jpg'
+import image65 from '@/assets/images/0L2A0967.jpg'
+import image66 from '@/assets/images/0L2A0979.jpg'
+import image67 from '@/assets/images/0L2A0983.jpg'
+import image68 from '@/assets/images/0L2A0992.jpg'
+import image69 from '@/assets/images/0L2A0993.jpg'
+import image70 from '@/assets/images/0L2A0995.jpg'
+import image71 from '@/assets/images/0L2A0996.jpg'
+import image72 from '@/assets/images/0L2A1001.jpg'
+import image73 from '@/assets/images/0L2A1018.jpg'
+import image74 from '@/assets/images/0L2A1026.jpg'
+import image75 from '@/assets/images/0L2A1050.jpg'
 
-const images = [   /* 이미지 경로를 리스트로 저장 */
-  {
-    alt: 'image01',
-    source: image01,
-    width: 640,
-    height: 960
-  },
-  {
-    alt: 'image02',
-    source: image02,
-    width: 640,
-    height: 960
-  },
-  {
-    alt: 'image03',
-    source: image03,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image04',
-    source: image04,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image05',
-    source: image05,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image06',
-    source: image06,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image07',
-    source: image07,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image08',
-    source: image08,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image09',
-    source: image09,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image10',
-    source: image10,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image11',
-    source: image11,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image12',
-    source: image12,
-    width: 640,
-    height: 960
-  }
+const images = [
+  { alt: 'image01', source: image01, width: 5350, height: 8021 },
+  { alt: 'image02', source: image02, width: 5350, height: 8021 },
+  { alt: 'image03', source: image03, width: 8021, height: 5350 },
+  { alt: 'image04', source: image04, width: 5350, height: 8021 },
+  { alt: 'image05', source: image05, width: 8021, height: 5350 },
+  { alt: 'image06', source: image06, width: 8021, height: 5350 },
+  { alt: 'image07', source: image07, width: 8021, height: 5350 },
+  { alt: 'image08', source: image08, width: 5350, height: 8021 },
+  { alt: 'image09', source: image09, width: 5350, height: 8021 },
+  { alt: 'image10', source: image10, width: 5350, height: 8021 },
+  { alt: 'image11', source: image11, width: 5350, height: 8021 },
+  { alt: 'image12', source: image12, width: 5350, height: 8021 },
+  { alt: 'image13', source: image13, width: 5350, height: 8021 },
+  { alt: 'image14', source: image14, width: 5350, height: 8021 },
+  { alt: 'image15', source: image15, width: 5350, height: 8021 },
+  { alt: 'image16', source: image16, width: 5350, height: 8021 },
+  { alt: 'image17', source: image17, width: 5350, height: 8021 },
+  { alt: 'image18', source: image18, width: 5350, height: 8021 },
+  { alt: 'image19', source: image19, width: 5350, height: 8021 },
+  { alt: 'image20', source: image20, width: 5350, height: 8021 },
+  { alt: 'image21', source: image21, width: 5350, height: 8021 },
+  { alt: 'image22', source: image22, width: 5350, height: 8021 },
+  { alt: 'image23', source: image23, width: 5350, height: 8021 },
+  { alt: 'image24', source: image24, width: 5350, height: 8021 },
+  { alt: 'image25', source: image25, width: 5350, height: 8021 },
+  { alt: 'image26', source: image26, width: 5350, height: 8021 },
+  { alt: 'image27', source: image27, width: 5350, height: 8021 },
+  { alt: 'image28', source: image28, width: 5350, height: 8021 },
+  { alt: 'image29', source: image29, width: 5350, height: 8021 },
+  { alt: 'image30', source: image30, width: 5350, height: 8021 },
+  { alt: 'image31', source: image31, width: 5350, height: 8021 },
+  { alt: 'image32', source: image32, width: 5350, height: 8021 },
+  { alt: 'image33', source: image33, width: 8021, height: 5350 },
+  { alt: 'image34', source: image34, width: 8021, height: 5350 },
+  { alt: 'image35', source: image35, width: 5350, height: 8021 },
+  { alt: 'image36', source: image36, width: 5350, height: 8021 },
+  { alt: 'image37', source: image37, width: 5350, height: 8021 },
+  { alt: 'image38', source: image38, width: 5350, height: 8021 },
+  { alt: 'image39', source: image39, width: 5350, height: 8021 },
+  { alt: 'image40', source: image40, width: 5350, height: 8021 },
+  { alt: 'image41', source: image41, width: 5350, height: 8021 },
+  { alt: 'image42', source: image42, width: 5350, height: 8021 },
+  { alt: 'image43', source: image43, width: 5350, height: 8021 },
+  { alt: 'image44', source: image44, width: 5350, height: 8021 },
+  { alt: 'image45', source: image45, width: 8021, height: 5350 },
+  { alt: 'image46', source: image46, width: 5350, height: 8021 },
+  { alt: 'image47', source: image47, width: 5350, height: 8021 },
+  { alt: 'image48', source: image48, width: 8021, height: 5350 },
+  { alt: 'image49', source: image49, width: 5350, height: 8021 },
+  { alt: 'image50', source: image50, width: 8021, height: 5350 },
+  { alt: 'image51', source: image51, width: 5350, height: 8021 },
+  { alt: 'image52', source: image52, width: 5350, height: 8021 },
+  { alt: 'image53', source: image53, width: 8021, height: 5350 },
+  { alt: 'image54', source: image54, width: 5350, height: 8021 },
+  { alt: 'image55', source: image55, width: 8021, height: 5350 },
+  { alt: 'image56', source: image56, width: 5350, height: 8021 },
+  { alt: 'image57', source: image57, width: 5350, height: 8021 },
+  { alt: 'image58', source: image58, width: 5350, height: 8021 },
+  { alt: 'image59', source: image59, width: 5350, height: 8021 },
+  { alt: 'image60', source: image60, width: 8021, height: 5350 },
+  { alt: 'image61', source: image61, width: 5350, height: 8021 },
+  { alt: 'image62', source: image62, width: 8021, height: 5350 },
+  { alt: 'image63', source: image63, width: 5350, height: 8021 },
+  { alt: 'image64', source: image64, width: 5350, height: 8021 },
+  { alt: 'image65', source: image65, width: 8021, height: 5350 },
+  { alt: 'image66', source: image66, width: 8021, height: 5350 },
+  { alt: 'image67', source: image67, width: 8021, height: 5350 },
+  { alt: 'image68', source: image68, width: 8021, height: 5350 },
+  { alt: 'image69', source: image69, width: 5350, height: 8021 },
+  { alt: 'image70', source: image70, width: 5350, height: 8021 },
+  { alt: 'image71', source: image71, width: 8021, height: 5350 },
+  { alt: 'image72', source: image72, width: 8021, height: 5350 },
+  { alt: 'image73', source: image73, width: 5350, height: 8021 },
+  { alt: 'image74', source: image74, width: 5350, height: 8021 },
+  { alt: 'image75', source: image75, width: 5350, height: 8021 }
 ];
 
 export default images;
--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   server: {
-    host: true,  // 或者 host: '0.0.0.0'
-    port: 3000,  // 你项目端口，默认3000可改
+    host: true, // 或者 host: '0.0.0.0'
+    port: 3000, // 你项目端口，默认3000可改
   },
   plugins: [react(), tsconfigPaths(), svgr()],
 });
